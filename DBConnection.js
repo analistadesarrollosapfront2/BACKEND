6ee763ae9f390d23820fa1e3b1f5fa62f33b5108
Erick11-prog <lrerick0802@gmail.com>
2023-10-01 18:00:17 -0600

    DEV_bd

--- DBConnection.js
+++ DBConnection.js
@@ -5,7 +5,7 @@ module.exports = {
   database: {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'nombre_usuario',
-    user: process.env.DB_PASSWORD || ''
+    user: process.env.DB_USER || 'root',
+    user: process.env.DB_PASSWORD || 'root',
   },
 };
